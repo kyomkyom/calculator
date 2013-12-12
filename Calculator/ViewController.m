bacca6ba1388e23a4c31cf66ede0f5deec5da411
kyomkyom <2011531017@ma213.is.env.kitakyu-u.ac.jp>
2013-12-12 14:05:08 +0900

    没

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -60,24 +60,22 @@
     count += 1;
     
     //現在値の保存及び演算    
-    if (count >= 2){
-     if( operation == 0){
-         currentValue += [numberLabel.text intValue];
-     } else if ( operation == 1){
-         currentValue -= currentValue - [numberLabel.text intValue];
-     } else if ( operation == 2){
-         if (count >= 2){
-             currentValue *= [numberLabel.text intValue];
-         } else {currentValue = [numberLabel.text intValue];
-             }
-     } else if ( operation == 3){
-         if (count >= 2){
-             currentValue /= [numberLabel.text intValue];
-         } else {currentValue = [numberLabel.text intValue];
-             }
-        }
-    } else {
-        currentValue = [numberLabel.text intValue];
+    
+    if( operation == 0){
+        if ()
+        currentValue += [numberLabel.text intValue];
+    } else if ( operation == 1){
+        currentValue -= currentValue - [numberLabel.text intValue];
+    } else if ( operation == 2){
+        if (count >= 2){
+            currentValue *= [numberLabel.text intValue];
+        } else {currentValue = [numberLabel.text intValue];
+            }
+    } else if ( operation == 3){
+        if (count >= 2){
+            currentValue /= [numberLabel.text intValue];
+        } else {currentValue = [numberLabel.text intValue];
+            }
     }
 
     //演算の保存
